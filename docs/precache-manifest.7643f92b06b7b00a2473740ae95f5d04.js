cd21a0ccaba7a23ebada6c6386faea4714542833
Orie Steele <orie@transmute.industries>
2020-03-10 18:31:13 -0500

    :rocket:

---
+++ docs/precache-manifest.7643f92b06b7b00a2473740ae95f5d04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2ee1af57cc37c6a553b01a373b3590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b613c44e819077d09dd",
+    "url": "/static/css/main.e8bca610.chunk.css"
+  },
+  {
+    "revision": "1f82314bf0c0208b7c7b",
+    "url": "/static/js/2.c234d204.chunk.js"
+  },
+  {
+    "revision": "4bbdddaa8bf2826231ec736c742b61e4",
+    "url": "/static/js/2.c234d204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b613c44e819077d09dd",
+    "url": "/static/js/main.7b3d3ff5.chunk.js"
+  },
+  {
+    "revision": "1a09079d96729cc4b56a",
+    "url": "/static/js/runtime-main.f1949b6f.js"
+  },
+  {
+    "revision": "7d03fa3680fc5c7b8dc15e23e14d740f",
+    "url": "/static/media/key.7d03fa36.svg"
+  },
+  {
+    "revision": "33b194d8ce06cb2e6794ecb444201773",
+    "url": "/static/media/logo.33b194d8.svg"
+  }
+]);
