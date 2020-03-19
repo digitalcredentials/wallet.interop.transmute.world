28a161214f1f09f3b0a585c51468845333cdf260
Orie Steele <orie@transmute.industries>
2020-03-19 10:40:49 -0500

    simplify

--- src/store/chapi/handlers.js
+++ src/store/chapi/handlers.js
@@ -4,33 +4,7 @@ import { compose } from 'recompose';
 import { v4 as uuidv4 } from 'uuid';
 
 const walletObjectToArray = (walletContents = {}) => {
-  let walletRows = [];
-  // let keys = Object.keys(walletContents);
-  Object.values(walletContents).forEach((v1) => {
-    if (v1.id) {
-      walletRows.push(v1);
-    } else {
-      // v1.id = key;
-      if (v1.verifiableCredential) {
-        v1.verifiableCredential.forEach((v2) => {
-          // v2.id = k2;
-          walletRows.push(v2);
-        })
-      } else {
-        walletRows.push(v1);
-      }
-    }
-  })
-
-  walletRows = walletRows.map((v, i) => {
-    if (!v.id) {
-      // v.id = keys[i]
-      // v.type = ['VerifiablePresentation', 'DIDAuth']
-    }
-    return v;
-  })
-  // console.log(walletRows);
-  return walletRows;
+  return Object.values(walletContents);
 }
 
 export default compose(
@@ -46,16 +20,12 @@ export default compose(
         });
       } else {
         walletContents = JSON.parse(atob(walletContents));
-        // console.log(walletContents)
         setChapiProp({ wallet: { isLoaded: true, object: walletContents } });
       }
     },
   }),
   withHandlers({
     storeInWallet: ({ chapi, loadWalletContents }) => (verifiablePresentation) => {
-
-      console.log(verifiablePresentation);
-
       let walletContents = chapi.wallet.object || {};
       let vc = verifiablePresentation;
       if (verifiablePresentation.verifiableCredential) {
@@ -64,11 +34,7 @@ export default compose(
           : verifiablePresentation.verifiableCredential;
       }
       const id = vc.id || `urn:uuid:${uuidv4()}`;
-
       walletContents[id] = vc;
-
-      console.log(walletContents);
-      // base64 encode the serialized contents (verifiable presentations)
       const serialized = btoa(JSON.stringify(walletContents));
       localStorage.setItem('walletContents', serialized);
       loadWalletContents();
